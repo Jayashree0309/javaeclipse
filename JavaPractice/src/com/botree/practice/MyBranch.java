bfe29b355c4e2e312805efb2dc81077fcf78fe42
nithyasri200206 <nithyasricse02@gmail.com>
2024-11-25 17:47:47 +0530

    mybranch request

---
+++ JavaPractice/src/com/botree/practice/MyBranch.java
@@ -0,0 +1,5 @@
+package com.botree.practice;
+
+public class MyBranch {
+
+}
--- JavaPractice/src/com/botree/practice/Sample.java
+++ JavaPractice/src/com/botree/practice/Sample.java
@@ -3,5 +3,7 @@ package com.botree.practice;
 public class Sample {
 public static void main(String[] args) {
 	System.out.println("demo");
+	
 }
+
 }
